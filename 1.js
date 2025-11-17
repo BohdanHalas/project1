b8f40e6aa1358e55a0db2368f45aebc1f4b18107
Bohdan <.email@example.com>
2025-11-17 20:23:44 +0100

    My 1st commit. Add 1.js and 2.js

---
+++ 1.js
@@ -0,0 +1,10 @@
+let age = 26;
+let Name = "Bohdan";
+let online = true;
+
+console.log(`Otse prikol ${age} Kapec tu daesh`);
+console.log(`Otse prikol`, age, `Kapec tu daesh`);
+console.log(`Tvoie imya ` + Name);
+console.log(`type of age: ${typeof age}`);
+console.log(`type of Name:`, typeof Name);
+console.log(`type of Online:, ${typeof online}. Znachennya online: ` + online);
---
+++ 2.js
@@ -0,0 +1,3 @@
+let fullName = "Bohdan Halas";
+let age = 25;
+let isStudent = false;
